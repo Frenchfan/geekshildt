7ab9698d18ab2fc1af4e8f7c4b5b822806651a61
Frenchfan <sumkin.gpb@gmail.com>
2022-07-29 04:19:08 +0300

    reverse

--- src/main/java/ru/geekshildt/GeekBrains/Reverse.java
+++ src/main/java/ru/geekshildt/GeekBrains/Reverse.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class Reverse {
 
     public static void main(String[] args) {
-        int[] myArray = new int[] {0, 1, 2, 3, 4, 5};
+        int[] myArray = new int[] {8, 2, 4, 3, 5, 9, 11};
         int temp;
-        for (int i = 0; i <= myArray.length / 2; i++) {
+        for (int i = 0; i < myArray.length / 2; i++) {
             temp = myArray[i];
             myArray[i] = myArray[myArray.length - 1 - i];
             myArray[myArray.length - 1 - i] = temp;
